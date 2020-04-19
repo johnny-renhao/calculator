991adb32d54f44290576a3be2490f8fc417b0311
Winston Xie <winston.xie@ringcentral.com>
2020-04-19 20:31:26 +0800

    real calculator

---
+++ precache-manifest.cb29005eccf7c958816359906cd2c802.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38887f51f11fc79c813ce098c58d3d48",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "f710a1e16c4a2ff47a6d",
+    "url": "/calculator/static/css/2.5510c45e.chunk.css"
+  },
+  {
+    "revision": "a501af14f44734707302",
+    "url": "/calculator/static/css/main.86f680a8.chunk.css"
+  },
+  {
+    "revision": "f710a1e16c4a2ff47a6d",
+    "url": "/calculator/static/js/2.ad41f766.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.ad41f766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a501af14f44734707302",
+    "url": "/calculator/static/js/main.c89a9525.chunk.js"
+  },
+  {
+    "revision": "552c96952084d63edc80",
+    "url": "/calculator/static/js/runtime-main.29434a71.js"
+  }
+]);
